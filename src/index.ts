960948fc2ac0d862c4e8d14990a69b2af5288d22
Theodor Dumitrache <theodor.dumitrache@amdaris.com>
2022-10-20 22:34:36 +0300

    -

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './hooks';
+export * from './services';
+export * from './config';
+export * from './utils';
---
+++ src/utils/hasValidAccessToken.ts
@@ -0,0 +1,11 @@
+export const hasValidAccessToken = () => {
+    if (localStorage.getItem('credentials')) {
+        const { accessTokenExpirationTime } = JSON.parse(localStorage.getItem('credentials')!);
+
+        if (accessTokenExpirationTime > Date.now() / 1000) {
+            return true;
+        }
+    }
+
+    return false;
+};
